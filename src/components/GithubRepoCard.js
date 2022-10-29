bdaeef61bce729009b26fe84f859160c6a535767
sushant-sg9 <sushantghadge2016@gmail.com>
2022-10-29 19:38:57 +0530

    first-commit

---
+++ src/components/GithubRepoCard.js
@@ -0,0 +1,43 @@
+import { StarOutlined, IssuesCloseOutlined } from "@ant-design/icons"
+import moment from "moment"
+import "../cssfile/githubrepo.css"
+
+
+const GithubRepoCard = (props) => {
+	//Get Repos Infos from Props
+	const { Item } = props
+	
+	const Optmize = (total) => {
+		if (total >= 1000)
+			total = (total / 1000).toFixed(1).toString() + "K";
+		return total;
+	}
+
+	return (
+		<div className="card">
+			<div className="thumb">
+				<img src={Item?.owner?.avatar_url} alt={Item?.gravatar_id} />
+			</div>
+			<div className="infos">
+				<div className="Title">{Item?.name}</div>
+				<div className="desc">{Item?.description}</div>
+				<div className="action">
+					<div className="details">
+						<StarOutlined /> Stars: {Optmize(Item?.stargazers_count)}
+					</div>
+					<div className="details">
+						<IssuesCloseOutlined /> Issues: {Optmize(Item?.open_issues_count)}
+					</div>
+					<div className="datesection">
+						<div className="date">
+							Submitted {moment(Item?.created_at).fromNow()} by {Item?.owner?.login}
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default GithubRepoCard
---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react"
+import { Empty, Spin } from "antd"
+import "../cssfile/Home.css"
+import GithubRepoCard from "./GithubRepoCard"
+import GithubRepos from "../services/GithubRepos"
+import InfiniteScroll from "react-infinite-scroll-component"
+import Heading from "./Heading"
+import Swal from 'sweetalert2'
+
+const Home = () => {
+	const [pageNumber, setPageNumber] = useState(1)
+	const [result, setResult] = useState([])
+	const [loader, setLoader] = useState(true)
+
+
+	useEffect(() => {
+		getGithubRepos()
+		// eslint-disable-next-line
+	}, [pageNumber])
+
+	//Get Github Repos first time
+	const getGithubRepos = () => {
+		GithubRepos.getGithubRepos(pageNumber)
+			.then(res => {
+				setLoader(false)
+				res?.items?.length && setResult([...result, ...res?.items])
+			})
+			.catch(err => {
+				setLoader(false)
+				Swal.fire({
+					title: 'Error!',
+					text: err?.message,
+					icon: 'error',
+					confirmButtonText: 'Ok'
+				})
+			})
+	}
+
+	const getNextPage = () => {
+		setPageNumber(pageNumber + 1)
+	}
+
+	return (
+		<div className="home">
+			<div className="head">
+				<Heading color="primary" text="Most followed Github repositories created in the last 30 days" />
+			</div>
+			{loader && <Spin className="loading" />}
+			{result?.length && !loader ? <div className="content">
+				<InfiniteScroll
+					dataLength={result?.length}
+					next={getNextPage}
+					loader={<Spin style={{ marginTop: "20px" }} />}
+					scrollableTarget="sessions"
+					scrollThreshold={1}
+					hasMore={true}
+				>
+					{result?.map((item) => {
+						return (<GithubRepoCard Item={item} key={item?.id} />)
+					})}
+				</InfiniteScroll>
+			</div> : <Empty style={{ marginTop: "100px" }} />}
+		</div>
+	)
+}
+
+
+export default Home
